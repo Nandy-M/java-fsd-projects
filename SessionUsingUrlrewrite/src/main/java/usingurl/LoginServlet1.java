d5ae3d055b619e373b057959bf5c2a7153caaa1f
Nandhini <nandhinimurugesan03@gmail.com>
2022-08-03 19:22:47 +0530

    found and fix

---
+++ SessionUsingUrlrewrite/src/main/java/usingurl/LoginServlet1.java
@@ -0,0 +1,30 @@
+package usingurl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet1
+ */
+@WebServlet("/LoginServlet1")
+public class LoginServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        String userId = request.getParameter("userid");
+       response.sendRedirect("dashboard?userid=" + userId);  
+       
+
+	}
+
+	}
